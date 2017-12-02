44823771717f3db8e990c6776958b28b0489f773
Dave Flanagan <dflanagan314@gmail.com>
2017-12-01 22:05:25 -0500

    Initial commit

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour {
+
+    public float tileXPos = 0;
+    public float tileYPos = 0;
+    public TileType tileType = TileType.Straight;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/TileType.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum TileType {
+    Straight, Left30, Right30
+}
