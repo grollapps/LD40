643ec55625d3d6ece8257c8fab6d4fd496931667
Dave Flanagan <dflanagan314@gmail.com>
2017-12-03 09:27:53 -0500

    Add checkpoint objects and setup layers for collisions

---
+++ Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    void OnTriggerEnter(Collider collider) {
+        Debug.Log(gameObject.name + ": Checkpoint hit " + collider.gameObject.name);
+    }
+
+}
